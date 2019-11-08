6ded6f7ae1ccdc538f02b10abc7048f37edbcf31
Christoffer <christoffer.ssj@gmail.com>
2019-11-08 15:06:52 +0100

    nothing really

--- git_it.sh
+++ git_it.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
-git add .
-git status
-echo The comment for git:
-read input
-git commit -m "$input"
-git push
+echo Starting git process.
+echo please input your rsa key for the project.
+read pass
+while :
+do
+	git add .
+	git status
+	echo The comment for git:
+	read input
+	git commit -m "$input"
+	git push
+done
